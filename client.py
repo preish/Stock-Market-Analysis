e189df1adfa75694b7547bf1bc7453dc699aa465
Juan Sulse <71233989+preish@users.noreply.github.com>
2024-08-30 11:51:52 -0500

    Plot NVDA data on candlestick graph

--- client.py
+++ client.py
@@ -3,6 +3,8 @@ from typing import cast # needed to cast HTTPResponse as JSON
 from urllib3 import HTTPResponse
 import config # config.py to get API_KEY
 import json   # because polygon returns the data from the API call in HTTPResponse format
+from plotly import graph_objects as go
+import pandas as pd
 
 
 client = RESTClient(config.API_KEY)
@@ -20,5 +22,33 @@ for item in data:
     if item == 'results':
         resultsData = data[item]
 
+
+# Capture data for graphing
+closeList = []
+openList = []
+highList = []
+lowList = []
+timeList = []
 for bar in resultsData:
-    print(bar)
+    for category in bar:
+        if category == 'c':
+            closeList.append(bar[category])
+        elif category == 'o':
+            openList.append(bar[category])
+        elif category == 'h':
+            highList.append(bar[category])
+        elif category == 'l':
+            lowList.append(bar[category])
+        elif category == 't':
+            timeList.append(bar[category])
+
+# Convert timestamp
+times = []
+for time in timeList:
+    times.append(pd.Timestamp(time, tz='GMT', unit='ms'))
+
+# Plot NVDA data (more info: https://plotly.com/python/candlestick-charts/)
+fig = go.Figure()
+fig.add_trace(go.Candlestick(x=times, open=openList, close=closeList, high=highList, low=lowList, name='NVDA Monthly 2023'))
+fig.update_layout(xaxis_rangeslider_visible=False, template="plotly_dark")
+fig.show() #opens a browser with the plotly stock graph (so cool)
