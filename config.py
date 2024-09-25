c5729b0631200a0e20b767c29c69ed3b6e00a2c3
Juan Sulse <71233989+preish@users.noreply.github.com>
2024-09-25 14:11:04 -0400

    Add text field and misc comments
    Added text field for ticker symbol search

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
+# Polygon.io API key 
 API_KEY = 'N2PPLhf6l6L59jun4EyHn21zSkXytq1u'
--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 import sys
+from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow #toolkit for GUI
 # from PyQt5.QtGui import QIcon
 
-# Configure app
 def window():
+    # App configurations
     app = QApplication(sys.argv)
     win = QMainWindow()
     win.setGeometry(1200,300,500,500) # Set starting location for app
     win.setWindowTitle('Stock Market Analysis')
     # win.setWindowIcon(QIcon('image.png')) # Change default window icon
+
+    # Ticker Field
+    lbl_ticker = QtWidgets.QLabel(win)
+    lbl_ticker.setText('Ticker Symbol: ')
+    lbl_ticker.move(50,50)
+    txt_ticker = QtWidgets.QLineEdit(win)
+    txt_ticker.move(200,50)
+
+    def clicked(self):
+        print(f'You entered the ticker symbol {txt_ticker.text().upper()}')
+
+    btn_save = QtWidgets.QPushButton(win)
+    btn_save.setText('Search')
+    btn_save.clicked.connect(clicked)
+    btn_save.move(200,100)
+
     win.show()
     sys.exit(app.exec_())
 
